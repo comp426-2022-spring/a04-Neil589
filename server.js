28dd7c718025672e1127f26918b01466e68e38e7
Neil Thupili <69744850+Neil589@users.noreply.github.com>
2022-04-17 05:03:41 -0400

    f

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.use( (req, res, next) => {
     }
 
     console.log(logdata);
-    const db_prep = db.prepare('INSERT INTO accesslog (remoteaddr,remoteuser,time,method,url,protocol,httpversion,status,referer,useragent) VALUES (?,?,?,?,?,?,?,?,?,?)')
-    const db_run = db_prep.run(logdata.remoteaddr,logdata.remoteuser, logdata.time, logdata.method,logdata.url,logdata.protocol,logdata.httpversion, logdata.satus,logdata.referer, logdata.useragent)
+    const prep = db.prepare('INSERT INTO accesslog (remoteaddr,remoteuser,time,method,url,protocol,httpversion,status,referer,useragent) VALUES (?,?,?,?,?,?,?,?,?,?)')
+    const run = prep.run(logdata.remoteaddr,logdata.remoteuser, logdata.time, logdata.method,logdata.url,logdata.protocol,logdata.httpversion, logdata.satus,logdata.referer, logdata.useragent)
     next();
 
     })
